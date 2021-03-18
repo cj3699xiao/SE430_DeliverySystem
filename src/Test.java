42bbc63b37b4c3f58eb74e89738c249cd40d5e52
cj3699xiao <354123302@qq.com>
2021-03-18 12:05:33 -0500

    update

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,10 @@
 public class Test {
     //this class is used for testing
     public static void main(String[] args) {
+
+        String test = "a";
+        test.split("\\.");
         String[] a = new String[]{"a","b"};
+
     }
 }
