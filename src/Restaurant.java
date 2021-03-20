b01adfb7f87016c7470d217d70c905bdc12a3e25
cj3699xiao <354123302@qq.com>
2021-03-20 00:03:25 -0500

    Restaurant journey update1

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Restaurant extends User{
     protected List<Dish> menu;
@@ -12,6 +13,43 @@ public class Restaurant extends User{
 
     @Override
     public void operations() {
+        Scanner input = new Scanner(System.in);
+        int oper = 0;
+
+        while (oper != 9) {
+            System.out.println("Hello, Restaurant " + super.getUserName() + ". Please select following options: ");
+            System.out.println("1. Show all restaurant orders");
+            System.out.println("2. Confirm order status to prepared");
+            System.out.println("3. Confirm order status to been picked up");
+            System.out.println("4. Report problem to Admin");
+            System.out.println("9. Quit");
+
+            String in = input.nextLine();
+
+            try {
+                oper = Integer.parseInt(in);
+            } catch (NumberFormatException e) {
+                System.out.println("Error! Please enter a valid operation!");
+                continue;
+            }
+            
+            if (oper == 1) {
+
+            } else if(oper == 2) {
+
+            } else if(oper == 3) {
+
+            } else if(oper == 4) {
+
+            } else if (oper == 9) {
+                break;
+            } else {
+                System.out.println("Error! Please enter a valid operation!");
+                continue;
+            }
+        }
+
+        input.close();
 
     }
 
